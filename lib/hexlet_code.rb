01740a797426d1b6cbf33b1460dc59835c54d294
Semyonov <s.semyonov@mercurydevelopment.com>
2023-03-26 15:56:08 +0200

    Step-1: Added tag builder

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -5,4 +5,16 @@ require_relative "hexlet_code/version"
 module HexletCode
   class Error < StandardError; end
   # Your code goes here...
+
+  class Tag
+    def self.build(name, **params)
+      attributes = params.map { |key, value| " #{key}=\"#{value}\"" }.join
+
+      if block_given?
+        "<#{name}#{attributes}>#{yield}</#{name}>"
+      else
+        "<#{name}#{attributes}>"
+      end
+    end
+  end
 end
