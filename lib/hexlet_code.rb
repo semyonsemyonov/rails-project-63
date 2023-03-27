9a38da3e3ffe9e5c23d233f7197cf1900f4ad70d
Semyon Semyonov <s.semyonov@mercurydevelopment.com>
2023-03-27 14:14:23 +0400

    Step 6: Rewrite to autoload.

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -1,16 +1,15 @@
 # frozen_string_literal: true
 
-require_relative "hexlet_code/version"
-require_relative "hexlet_code/form_builder"
-
 # Form generator
 module HexletCode
+  autoload(:FormBuilder, "../lib/hexlet_code/form_builder")
+  autoload(:Input, "../lib/hexlet_code/form_builder")
+  autoload(:Submit, "../lib/hexlet_code/form_builder")
+  autoload(:Tag, "../lib/hexlet_code/form_builder")
+  autoload(:VERSION, "../lib/hexlet_code/version")
+
   class Error < StandardError; end
   # Your code goes here...
-  autoload(:FormBuilder, "../lib/hexlet_code/form_builder")
-  autoload(:Tag, "../lib/hexlet_code/tag")
-  autoload(:Submit, "../lib/hexlet_code/submit")
-  autoload(:Input, "../lib/hexlet_code/input")
 
   def self.form_for(entity, opts = {}, &block)
     FormBuilder.new(entity, opts).build(&block)
