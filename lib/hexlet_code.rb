fb36404dabe94f2c6f910d432f36d78905e0b61c
Semyon Semyonov <s.semyonov@mercurydevelopment.com>
2023-03-27 14:01:39 +0400

    Step 6: Rewrite to autoload.

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -7,6 +7,10 @@ require_relative "hexlet_code/form_builder"
 module HexletCode
   class Error < StandardError; end
   # Your code goes here...
+  autoload(:FormBuilder, "../lib/hexlet_code/form_builder")
+  autoload(:Tag, "../lib/hexlet_code/tag")
+  autoload(:Submit, "../lib/hexlet_code/submit")
+  autoload(:Input, "../lib/hexlet_code/input")
 
   def self.form_for(entity, opts = {}, &block)
     FormBuilder.new(entity, opts).build(&block)
