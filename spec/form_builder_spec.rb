ae05d27ef3b3bdef0e61bd47af51be22d9ee39bf
Semyon Semyonov <s.semyonov@mercurydevelopment.com>
2023-03-27 21:54:40 +0400

    Step 6: Rubocop fixes for specs.

--- spec/form_builder_spec.rb
+++ spec/form_builder_spec.rb
@@ -3,113 +3,113 @@
 User = Struct.new(:name, :job, :gender, keyword_init: true)
 
 RSpec.describe HexletCode do
-  context "generate form" do
-    it "create empty form" do
+  context 'generate form' do
+    it 'create empty form' do
       user = User.new
       form = HexletCode.form_for user
 
-      expect(form).to eq(fixture("empty_form"))
+      expect(form).to eq(fixture('empty_form'))
     end
 
-    it "create form with url" do
+    it 'create form with url' do
       user = User.new
-      form = HexletCode.form_for user, { url: "/users" }
+      form = HexletCode.form_for user, { url: '/users' }
 
-      expect(form).to eq(fixture("empty_form_with_url"))
+      expect(form).to eq(fixture('empty_form_with_url'))
     end
   end
 end
 
 RSpec.describe HexletCode do
-  context "generate input" do
-    it "create form with input" do
+  context 'generate input' do
+    it 'create form with input' do
       user = User.new
-      form = HexletCode.form_for(user, { url: "/users" }) { |f| f.input :name }
+      form = HexletCode.form_for(user, { url: '/users' }) { |f| f.input :name }
 
-      expect(form).to eq(fixture("form_with_input"))
+      expect(form).to eq(fixture('form_with_input'))
     end
 
-    it "create form with input classes" do
+    it 'create form with input classes' do
       user = User.new
-      form = HexletCode.form_for(user, { url: "/users" }) { |f| f.input :name, class: "user-input" }
+      form = HexletCode.form_for(user, { url: '/users' }) { |f| f.input :name, class: 'user-input' }
 
-      expect(form).to eq(fixture("input_with_classes"))
+      expect(form).to eq(fixture('input_with_classes'))
     end
 
-    it "create form with 2 inputs" do
+    it 'create form with 2 inputs' do
       user = User.new
-      form = HexletCode.form_for(user, { url: "/users" }) do |f|
-        f.input :name, class: "1"
-        f.input :job, class: "2"
+      form = HexletCode.form_for(user, { url: '/users' }) do |f|
+        f.input :name, class: '1'
+        f.input :job, class: '2'
       end
 
-      expect(form).to eq(fixture("form_with_2_inputs"))
+      expect(form).to eq(fixture('form_with_2_inputs'))
     end
   end
 end
 
 RSpec.describe HexletCode do
-  context "generate textarea" do
-    it "create form with textarea" do
+  context 'generate textarea' do
+    it 'create form with textarea' do
       user = User.new
-      form = HexletCode.form_for(user, { url: "/users" }) { |f| f.input :job, as: :text }
+      form = HexletCode.form_for(user, { url: '/users' }) { |f| f.input :job, as: :text }
 
-      expect(form).to eq(fixture("form_with_textarea"))
+      expect(form).to eq(fixture('form_with_textarea'))
     end
   end
 end
 
 RSpec.describe HexletCode do
-  context "generate input" do
-    it "with default value" do
-      user = User.new(name: "rob")
-      form = HexletCode.form_for(user, { url: "/users" }) { |f| f.input :name }
+  context 'generate input' do
+    it 'with default value' do
+      user = User.new(name: 'rob')
+      form = HexletCode.form_for(user, { url: '/users' }) { |f| f.input :name }
 
-      expect(form).to eq(fixture("input_with_default_value"))
+      expect(form).to eq(fixture('input_with_default_value'))
     end
 
-    it "with override text" do
-      user = User.new(name: "rob", job: "developer")
-      form = HexletCode.form_for(user, { url: "/users" }) { |f| f.input :job, as: :text }
+    it 'with override text' do
+      user = User.new(name: 'rob', job: 'developer')
+      form = HexletCode.form_for(user, { url: '/users' }) { |f| f.input :job, as: :text }
 
-      expect(form).to eq(fixture("input_with_override_text"))
+      expect(form).to eq(fixture('input_with_override_text'))
     end
   end
 end
 
 RSpec.describe HexletCode do
-  context "generate submit" do
-    it "with default value" do
-      user = User.new(name: "rob")
-      form = HexletCode.form_for(user, { url: "/users" }) do |f|
+  context 'generate submit' do
+    it 'with default value' do
+      user = User.new(name: 'rob')
+      form = HexletCode.form_for(user, { url: '/users' }) do |f|
         f.input :name
         f.submit
       end
 
-      expect(form).to eq(fixture("with_submit_with_default"))
+      expect(form).to eq(fixture('with_submit_with_default'))
     end
 
-    it "with override parameters" do
-      user = User.new(name: "rob", job: "developer")
-      form = HexletCode.form_for(user, { url: "/users" }) do |f|
+    it 'with override parameters' do
+      user = User.new(name: 'rob', job: 'developer')
+      form = HexletCode.form_for(user, { url: '/users' }) do |f|
         f.input :name
         f.submit :Wow
       end
-      expect(form).to eq(fixture("with_submit_with_override"))
+      expect(form).to eq(fixture('with_submit_with_override'))
     end
   end
 end
 
 RSpec.describe HexletCode do
-  context "generate label" do
-    it "with default name" do
-      user = User.new(name: "rob")
-      form = HexletCode.form_for(user, { url: "/users" }) do |f|
+  context 'generate label' do
+    it 'with default name' do
+      user = User.new(name: 'rob')
+      form = HexletCode.form_for(user, { url: '/users' }) do |f|
         f.input :name
         f.submit
       end
 
-      expect(form).to eq(fixture("input_with_label"))
+      expect(form).to eq(fixture('input_with_label'))
     end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
-path = File.expand_path(File.join(File.dirname(__FILE__), "../lib"))
+path = File.expand_path(File.join(File.dirname(__FILE__), '../lib'))
 $LOAD_PATH << path
 
-Dir[File.join(path, "**/*.rb")].each { |file| require file }
+Dir[File.join(path, '**/*.rb')].each { |file| require file }
 
 module SpecHelpers
   def fixture(name)
-    File.read(Dir["spec/fixtures/*"].find { |f| File.basename(f, ".html") == name })
+    File.read(Dir['spec/fixtures/*'].find { |f| File.basename(f, '.html') == name })
   end
 end
 
